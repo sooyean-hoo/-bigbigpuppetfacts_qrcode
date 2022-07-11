40b65fc167cc82045959ddd782dda31dd0d0d9a4
ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2022-07-11 18:22:44 +0800

    do an check at an intervals
    Signed-off-by: ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

--- PEConsoleQRCodeHelper.user.js
+++ PEConsoleQRCodeHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PEConsoleQRCodeHelper
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0.0.1
+// @version      0.1.0.0.2
 // @description  For TamperMonkey or GreaseMonkey ... a helper to make the QR Code display nicer in the Puppet Enterprise Console
 // @author       Hoo Sooyean 何書淵
 // @connect      *
@@ -17,25 +17,32 @@
 // @downloadURL  https://openuserjs.org/install/Sooyean-hoo/PEConsoleQRCodeHelper.user.js
 // ==/UserScript==
 
+const qrcorrect =(f)=>{
+
+	if (location.href.indexOf('/inventory/node') ==  -1 ) return ;
+	if (location.href.indexOf('facts') ==  -1 ) return ;
+
+	if ($("a[href *= '/inventory/node/' ]").length == 0 ) return ;
+	if ($("a[href *= facts]").length == 0 ) return ;
+
+    let nodes=document.querySelectorAll(".node-fact-value")
+
+    for( let index=0; index < nodes.length ; index++){
+        let n = nodes[index];
+        if ( n.innerText.startsWith( '█' ) ){
+            n.style.lineHeight='1.2ch' ;
+        }
+    }
+}
+
 (function() {
     'use strict';
 
-    setTimeout( f=>{
-
-    	if (location.href.indexOf('/inventory/node') ==  -1 ) return ;
-    	if (location.href.indexOf('facts') ==  -1 ) return ;
-
-    	if ($("a[href *= '/inventory/node/' ]").length == 0 ) return ;
-    	if ($("a[href *= facts]").length == 0 ) return ;
-
-        let nodes=document.querySelectorAll(".node-fact-value")
-
-        for( let index=0; index < nodes.length ; index++){
-            let n = nodes[index];
-            if ( n.innerText.startsWith( '█' ) ){
-                n.style.lineHeight='1.2ch' ;
-            }
-        }
-    }, 5000)
+    setTimeout( qrcorrect , 5000)
     // Your code here...
+	if ($("a[href *= '/inventory/node/' ]").length == 0 ) return ;
+	if ($("a[href *= facts]").length == 0 ) return ;
+	setInterval(  qrcorrect   , 30000);
+
+
 })();
